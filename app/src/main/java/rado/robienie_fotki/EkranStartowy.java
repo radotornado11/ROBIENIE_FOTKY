e6a2b9511d5c311d3d96a5b7ef84cf8cb62234c9
Jacekiler <jacsz123@gmail.com>
2017-06-22 16:44:21 +0200

    .

--- app/src/main/java/rado/robienie_fotki/EkranStartowy.java
+++ app/src/main/java/rado/robienie_fotki/EkranStartowy.java
@@ -16,6 +16,7 @@ public class EkranStartowy extends AppCompatActivity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ekran_startowy);
 
